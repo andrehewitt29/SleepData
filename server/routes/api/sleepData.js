017cd0c470ba291f0e5275376c5e2176152840c1
AnnoyingTeemo <mjbwighton@gmail.com>
2023-05-10 19:58:50 +1200

    can now post data to firebase

--- server/routes/api/sleepData.js
+++ server/routes/api/sleepData.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const data = require("../../db/config");
+const userData = require("../../db/config");
 
 router.get('/', async (req,res) => {
-    const snapshot = await  data.get()
+    const snapshot = await userData.get()
     const list = snapshot.docs.map((doc)=>doc.data())
     res.send(list)    
+});
+
+router.post('/add', (req, res) => {
+    const data = req.body;
+    userData.add({ data });
+    res.send({ msg: data});
 });
 
 
