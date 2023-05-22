8af54c1a65efc281a18aeb73f62ff6a2c8dd784c
Andre <andreh1967@windowslive.com>
2023-05-22 13:00:38 +1200

    Setting up routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,16 @@
-//import React, {useEffect, useState} from 'react'
-//import ReactDOM from "react-dom";
-//import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-/*import NoPage from "./pages/NoPage";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-*/
-/*function App(){
+import {BrowserRouter, Routes, Route} from 'React-router-dom'
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
 
-  const [backendData, setBackendData] = useState([{}]);
-
-  useEffect(() => {
-  fetch("/api").then(
-    response => response.json()
-  ).then(
-    data =>{
-      setBackendData(data)
-    }
-  )
-  }, [])
-
-  return(
-    <div>
-      {(typeof backendData.users === 'undefined') ? (
-        <p>Loading...</p>
-      ):(
-        backendData.users.map((user, i) => (
-          <p>{user}</p>
-        ))
-      )}
-    </div>
-  )
-}*/
-
-function App() {
+export default function App() {
   return (
-    {Layout}
-    {Home}
-  );
+    <div> 
+    <BrowserRouter>
+    <Routes>
+      <Route index element={<Home/>}></Route>
+    </Routes>
+    <BrowserRouter/>
+    <div/>
+  )
 }
-
-export default App
