ee1e66afe0e6d0de9003d505f1355f039f62b2c4
Andre <andreh1967@windowslive.com>
2023-05-22 13:12:57 +1200

    All page links working

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
-import {BrowserRouter, Routes, Route} from 'React-router-dom'
-import Home from "./pages/Home"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import NoPage from "./pages/NoPage";
+
 
 export default function App() {
   return (
-    <div> 
-    <BrowserRouter>
-    <Routes>
-      <Route index element={<Home/>}></Route>
+    <div>
+      <BrowserRouter>
+      <Routes>
+      <Route index element={<Home/>}/>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/signin" element={<SignIn/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+      <Route path="/nopage" element={<NoPage/>}/>
     </Routes>
-    <BrowserRouter/>
-    <div/>
+      </BrowserRouter>     
+    </div>
   )
 }
