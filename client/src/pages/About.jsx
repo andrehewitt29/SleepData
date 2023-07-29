59640358e64ac9392c1e9847500f841859346ed8
cns3382 <83584777+cns3382@users.noreply.github.com>
2023-07-29 16:14:03 +1200

    Many Changes

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import '../style.css';
 
 function About() {
     return (
         <div class="container-fluid">
-            <Header />
             <br />
             <div style={{textAlign: "center"}}>
                 <h1>Our Mission</h1>
@@ -16,7 +12,6 @@ function About() {
                 <p>This service provides user with the ability to track their sleep data and habits over time, as well as </p>
             </div>
             <br />
-            <Footer />
         </div>
     );
 }
--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,9 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import Header from './Header';
-
-import Footer from './Footer';
-import '../style.css';
-import { NavLink, useNavigate } from 'react-router-dom';
 import {auth} from "../firebase";
 import placeholdImg from '../img/exampleStats.png';
 
@@ -12,38 +7,20 @@ function Account() {
     if(auth.currentUser != null){
         return (
             <div class="container-fluid">
-           
-           <br />
-            
-            <br />
-            <div class="row">
-                <div class="col-md-2" />
-                
-                <div class="col-md-2" />
-                <Header />
-                <form >
-                    <div>
-                    <ul id="rightHeader">
-        </ul>
-                    </div>
-                </form>  
-                <br/><br/>
-
                 <h1 style={{textAlign: "center"}}><strong>User Sleep Data:</strong></h1>
-
-                <div class="col-md-2">
-                    <a href="Settings" id="settingsButton"><button class="btn btn-primary">Settings</button></a>
+                <div class="row">
+                  <div class="col-md-2" />
+                  <div class="col-md-2">
+                      <a href="Settings" id="settingsButton"><button class="btn btn-primary">Settings</button></a>
+                  </div>
+                  <div class="col-md-4" style={{textAlign: "center"}}>
+                      <a href="DataForm" id="dataFormButton"><button class="btn btn-primary">Modify or Add Sleep Data</button></a>
+                  </div>
+                  <div class="col-md-2" style={{textAlign: "right"}}>
+                      <a href="Payment" id="paymentButton"><button class="btn btn-primary">Payment</button></a>
+                  </div>
+                  <div class="col-md-2" />
                 </div>
-                <div class="col-md-4" style={{textAlign: "center"}}>
-                    <a href="DataForm" id="dataFormButton"><button class="btn btn-primary">Modify or Add Sleep Data</button></a>
-                </div>
-                <div class="col-md-2" style={{textAlign: "right"}}>
-                    <a href="Payment" id="paymentButton"><button class="btn btn-primary">Payment</button></a>
-                </div>
-                This is the logged in account page, 
-                Users will have options to enter review or delete sleep data.
-                <Footer />
-            </div>
             <div class="row">
                 <div class="col-md-2" />
                 <div class="col-md-8">
@@ -52,20 +29,14 @@ function Account() {
                 <div class="col-md-2" />
             </div>
             <br />
-            <Footer />
         </div>
         )   
-    }else{
-        return(
+      }
+      return (
             <div class="container-fluid">
-                <Header />
-                Must Be Logged In to access this page.
-                <a href="Login" class="headlink" id="login"><li>Login Page</li></a>
-
-                <Footer />
+                <h1 style={{textAlign: "center"}}>Must Be Logged In to access this page.</h1>
             </div>
         )
-    }
 }
 
 export default Account;
