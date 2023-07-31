f97b8e723884f7a29334c0be637c66f9f4d7451e
Marson Beluso <wnp5030@aut.ac.nz>
2023-08-01 06:47:42 +1200

    Main Menu Updated V1
    Tuesday (1st August) prior to the meeting.
    
    Updated the main index page and replacing it with the following design, WIP.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import placeholdImg from '../img/card_placeholder.png';
 import dogImg from '../img/sleepingDog.jpg';
+import pieChart from '../img/pie_graph_icon.png';
+import peopleIcon from '../img/people_icon.png';
 
 function Home() {
     return (
@@ -40,15 +42,26 @@ function Home() {
         //         </div>
         //     </div>
         // </div>
-        <div class="two-box">
-            <div class="height-image"><img src={dogImg}/></div>
-            <div class="text-box">
-                <div class="sub-box">
-                    <h1>The Importance of Sleep</h1>
-                    <p>Try to maintain that cozy lifestyle by giving you enough shuteye to provide you many benefits!</p>
-                    <a href="SignUp" id="try-now">Sign Up</a>
+        <div id="index-page">
+            <div class="two-box">
+                <div class="sub-box" id="two-section">
+                    <div id="title-box">
+                        <h1>The Importance of Sleep</h1>
+                        <p>Try to maintain that cozy lifestyle by giving you enough shuteye to provide you many benefits!</p>
+                        <a id="signup-btn" href="SignUp">Sign Up</a>
+                    </div>
+                    <div id="description-box">
+                        <div>
+                            <img src={pieChart}/>
+                            <p>Scores can help track your records monthly and automatically generate statistics and graphs for your comparisons</p>
+                        </div>
+                        <div>
+                            <img src={peopleIcon}/>
+                            <p>By signing up an account, you can join online meetings <span id="highlight">LIVE</span> with the book writer Brian McKeown and his associates.</p>
+                        </div>
+                    </div>
                 </div>
-                <div class="sub-box"></div>
+                <div class="sub-box"><img id="dogtag" src={dogImg}/></div>
             </div>
         </div>
     );
