0e2584384dd919144da51cb2aec0611603843942
xmp4998 <83616948+xmp4998@users.noreply.github.com>
2023-08-01 12:48:27 +1200

    Update ForgotPassword.jsx

--- client/src/pages/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import {React } from 'react';
+import emailjs from '@emailjs/browser';
 import {auth} from "../firebase";
 
-function ForgotPassword() {
-    if (auth.currentUser != null) {
-        return (
-            <div class="container-fluid">
-                <h1 style={{textAlign: "center"}}>You are already logged in!</h1>
-            </div>
-        );
+export default function ForgotPassword(){
+
+    function authUser(e) {
+        if (auth.currentUser == null) {
+            const testEmail = document.getElementById("inputForm");
+            console.log(testEmail);
+            sendEmail(testEmail);
+        }
+        else{
+            document.getElementById('test').innerHTML = "Email would not send";
+        }
+        
+    }   
+
+    function sendEmail(e) {
+        emailjs.sendForm('service_fd21fn8', 'template_tn8fwr5', e, 'aYWGmhYGv5UUon7N6')
+        .then((result) => {
+            document.getElementById('test').innerHTML = 'Success';
+            console.log(result.text);
+        }, (error) => {
+            document.getElementById('test').innerHTML = 'Fail to send. Please contact the owner';
+            console.log(error.text);
+        });
+        e.target.reset();
     }
 
     return (
@@ -16,19 +34,18 @@ function ForgotPassword() {
             <div class="row" style={{minHeight: "150px", padding:"25px"}}>
                 <div class="col-md-4"/>
                 <div class="col-md-4">
-                    <form>
+                    <form id = "inputForm" onSubmit={authUser}>
                         <div class="form-group">
                             <label for="inputResetEmail">Please input your email: </label>
-                            <input type="email" class="form-control" id="inputResetEmail" />
+                            <input type="email" class="form-control" name = "email" id="inputResetEmail" required/>
                         </div>
                         <br />
-                        <button type="submit" class="btn btn-primary">Send Email</button>
+                        <button type="button" class="btn btn-primary" onClick={authUser}>Send Email</button>
                     </form>
                 </div>
                 <div class="col-md-4"/>
+                <p id = "test"></p>
             </div>
         </div>
     );
 }
-
-export default ForgotPassword;
