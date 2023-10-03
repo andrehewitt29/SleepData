3fed645a6a957bb612376f3a0246ebcf9f924e86
cns3382 <83584777+cns3382@users.noreply.github.com>
2023-10-03 16:21:31 +1300

    Proper signup issue fix

---
+++ client/src/pages/AccountCreated.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AccountCreated() {
+    return (
+        <div class="container-fluid error-page">
+            <h1>Account Created!</h1>
+        </div>
+    );
+}
+
+export default AccountCreated;
