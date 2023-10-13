6c04e679f3bbb735a0512e3f4f607cf286e8ebd3
cns3382 <83584777+cns3382@users.noreply.github.com>
2023-10-13 15:06:52 +1300

    Made search by value require an input

--- client/src/pages/AdminSearchByValue.jsx
+++ client/src/pages/AdminSearchByValue.jsx
@@ -3,47 +3,51 @@ import React from 'react';
 function SearchByValue() {
 
     async function loadUserData() {
-        document.getElementById("info").innerHTML = "<br />Loading...";
-
-        var tableContents = "<tr><td>Name</td><td>User ID</td><td>Date Submitted</td><td>Value</td></tr>";
         var input = document.getElementById("userValueInput").value;
 
-        var UIDDataJson = await fetch('http://localhost:5000/api/sleepData/AllUserUID', {
-        method: 'POST',
-        headers: { 'Content-Type' : 'application/json'}}
-        )
-        var UIDList = "";
-        await UIDDataJson.json().then(result => UIDList = result);
+        if (input != "") {
+            document.getElementById("info").innerHTML = "<br />Loading...";
 
-        for (var i = 0; i < UIDList.length; i++) {
-            var settingsDataJson = await fetch('http://localhost:5000/api/sleepData/getSettingsWithUid', {
+            var tableContents = "<tr><td>Name</td><td>User ID</td><td>Date Submitted</td><td>Value</td></tr>";
+            
+
+            var UIDDataJson = await fetch('http://localhost:5000/api/sleepData/AllUserUID', {
             method: 'POST',
-            headers: { 'Content-Type' : 'application/json'},
-            body: JSON.stringify(
-                {"user": UIDList[i]._queryOptions.collectionId}
-            )}
+            headers: { 'Content-Type' : 'application/json'}}
             )
-            var settingsDataList = "";
-            await settingsDataJson.json().then(result => settingsDataList = result);
+            var UIDList = "";
+            await UIDDataJson.json().then(result => UIDList = result);
 
-            var dataJson = await fetch('http://localhost:5000/api/sleepData/getWithUid', {
-            method: 'POST',
-            headers: { 'Content-Type' : 'application/json'},
-            body: JSON.stringify(
-                {"user": UIDList[i]._queryOptions.collectionId}
-            )}
-            )
-            var dataList = "";
-            await dataJson.json().then(result => dataList = result);
+            for (var i = 0; i < UIDList.length; i++) {
+                var settingsDataJson = await fetch('http://localhost:5000/api/sleepData/getSettingsWithUid', {
+                method: 'POST',
+                headers: { 'Content-Type' : 'application/json'},
+                body: JSON.stringify(
+                    {"user": UIDList[i]._queryOptions.collectionId}
+                )}
+                )
+                var settingsDataList = "";
+                await settingsDataJson.json().then(result => settingsDataList = result);
 
-            for (var a = 0; a < dataList.length; a++) {
-                tableContents = tableContents + "<tr><td>" + settingsDataList[0].firstNameValue + " " + settingsDataList[0].lastNameValue + "</td><td>" + UIDList[i]._queryOptions.collectionId + "</td><td>" + dataList[a].userInputDate + "</td><td>" + dataList[a][input] + "</td></tr>"
+                var dataJson = await fetch('http://localhost:5000/api/sleepData/getWithUid', {
+                method: 'POST',
+                headers: { 'Content-Type' : 'application/json'},
+                body: JSON.stringify(
+                    {"user": UIDList[i]._queryOptions.collectionId}
+                )}
+                )
+                var dataList = "";
+                await dataJson.json().then(result => dataList = result);
+
+                for (var a = 0; a < dataList.length; a++) {
+                    tableContents = tableContents + "<tr><td>" + settingsDataList[0].firstNameValue + " " + settingsDataList[0].lastNameValue + "</td><td>" + UIDList[i]._queryOptions.collectionId + "</td><td>" + dataList[a].userInputDate + "</td><td>" + dataList[a][input] + "</td></tr>"
+                }
             }
-        }
 
-        document.getElementById("results").innerHTML = tableContents;
-        document.getElementById("info").innerHTML = "";
-        document.getElementById("resultsBlock").hidden = false;
+            document.getElementById("results").innerHTML = tableContents;
+            document.getElementById("info").innerHTML = "";
+            document.getElementById("resultsBlock").hidden = false;
+        }
     }
 
     return (
