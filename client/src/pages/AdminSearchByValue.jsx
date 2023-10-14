65f2905cc892090ef23afa221171314e53d65c26
xmp4998 <83616948+xmp4998@users.noreply.github.com>
2023-10-14 18:02:55 +1300

    Update AdminSearchByValue.jsx
    Change form <input> tag to <select> tag,
    
    Button is in different line need to be fix

--- client/src/pages/AdminSearchByValue.jsx
+++ client/src/pages/AdminSearchByValue.jsx
@@ -54,7 +54,49 @@ function SearchByValue() {
         <div class="container-fluid">
             <h1>Search By Value</h1>
             <div class="form-background">
-                <input id="userValueInput" /><input id="userValueSearch" class="btn btn-primary" type="button" value="Search" onClick={loadUserData}/>
+            <select id = "userValueInput" name='userValueInput' style={{color: "black", display: 'block'}}>
+                <option disabled selected value> -- Select an Value -- </option>
+                    <option value = "readValue">Read Value</option>
+                    <option value = "readNonFictionValue">Read Non Fiction Value</option>
+                    <option value = "readEnjoymentValue">Read Enjoyment Value</option>
+                    <option value = "luckValue">Luck Value</option>
+                    <option value = "wellbeingValue">Wellbeing Value</option>
+                    <option value = "stressValue">Stress Value</option>
+                    <option value = "sleepValue">Sleep Value</option>
+                    <option value = "sleepHourValue">Sleep Hour Value</option>
+                    <option value = "fallAsleepValue">Fall Asleep Value</option>
+                    <option value = "awakenValue">Awaken Value</option>
+                    <option value = "chronotypeValue">Chronotype Value</option>
+                    <option value = "exerciseValue">Exercise Value</option>
+                    <option value = "snoreValue">Snore Value</option>
+                    <option value = "partnerSnoreValue">Partner Snore value</option>
+                    <option value = "petValue">Disturbed by Pet</option>
+                    <option value = "addictiveValue">Addictive Personality</option>
+                    <option value = "viaBestOneValue">Best VIA Value</option>
+                    <option value = "viaBestTwoValue">2nd Best VIA Value</option>
+                    <option value = "viaBestThreeValue">3rd Best VIA Value</option>
+                    <option value = "viaBestFourValue">4th Best VIA Value</option>
+                    <option value = "viaBestFiveValue">5th Best VIA Value</option>
+                    <option value = "viaLowestFiveValue">5th Lowest VIA Value</option>
+                    <option value = "viaLowestFourValue">4th Lowest VIA Value</option>
+                    <option value = "viaBestThreeValue">3rd Lowest VIA Value</option>
+                    <option value = "viaBestTwoValue">2nd Lowest VIA Value</option>
+                    <option value = "viaLowestOneValue">Lowest VIA Value</option>
+                    <option value = "gritValue">Grit Value</option>
+                    <option value = "gritPercentage">Grit Percentage</option>
+                    <option value = "physicalTouchPercentage">Physical Touch Percentage</option>
+                    <option value = "qualityTimePercentage">Quality Time Percentage</option>
+                    <option value = "wordsOfAffirmationPercentage">Words Of Affirmation Percentage</option>
+                    <option value = "actsOfServicePercentage">Acts Of Service Percentage</option>
+                    <option value = "receivingGiftsPercentage">Receiving Gifts Percentage</option>
+                    <option value = "exerciseValue">Exercise Mins</option>
+                    <option value = "fitnessValue">Fitness Score</option>
+                    <option value = "caffeinatedValue">Caffeinated Each Day</option>
+                    <option value = "caffeinatedDateValue">Last Caffeinated Date</option>
+                    <option value = "alcoholValue">Alcohol Day</option>
+                    
+                </select>
+                <input id="userValueSearch" class="btn btn-primary" type="button" value="Search" onClick={loadUserData}/>
                 <strong><h1 id= "info" ></h1></strong>
             </div>
             <br />
