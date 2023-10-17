35b9b56645a4d219ce71658da07d0c53d7cd6c38
cns3382 <83584777+cns3382@users.noreply.github.com>
2023-10-17 14:33:37 +1300

    Updated sign up again for more detail

--- client/src/pages/AccountCreated.jsx
+++ client/src/pages/AccountCreated.jsx
@@ -5,6 +5,7 @@ function AccountCreated() {
         <div class="container-fluid error-page">
             <h1>Account Created!</h1>
             <h5>Redirecting now...</h5>
+            <h5>(This might take a moment!)</h5>
         </div>
     );
 }
